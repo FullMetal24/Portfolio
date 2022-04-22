a5ec62e76ad96147c27352794ef6a7f13b8600eb
이삭 <alwlsdydtjd1@gmail.com>
2022-04-22 13:33:28 +0900

    이삭 : 시작 레벨 변경 업데이트

--- Portfolio/GameEngineContents/PuyoPuyo.cpp
+++ Portfolio/GameEngineContents/PuyoPuyo.cpp
@@ -72,7 +72,7 @@ void PuyoPuyo::GameInit()
 	CreateLevel<InGame>("InGame");
 	CreateLevel<GameOver>("GameOver");
 
-	ChangeLevel("EnemySelect");
+	ChangeLevel("Title");
 }
 
 void PuyoPuyo::GameLoop()
